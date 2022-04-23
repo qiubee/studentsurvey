aa844c9f9019645e9c0cb573a3183f978d7337e8
qiubee <45637654+qiubee@users.noreply.github.com>
2022-04-23 20:10:23 +0200

    use subrouter & cors, update endpoint handler

--- server/api/api.go
+++ server/api/api.go
@@ -4,12 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func HandleQuestions(w http.ResponseWriter, r *http.Request) {
+func saveAnswers(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(r.ParseForm())
+}
+
+func Endpoints(r *mux.Router) {
+	api := r.PathPrefix("/api/v1").Subrouter()
+	api.HandleFunc("/answers", saveAnswers).Methods(http.MethodPost, http.MethodOptions)
+	api.Use(mux.CORSMethodMiddleware(r))
 }
