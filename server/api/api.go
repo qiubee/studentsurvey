62740178fd1026c09353d26562b46713a2fe80e3
qiubee <45637654+qiubee@users.noreply.github.com>
2022-05-01 21:10:21 +0200

    add & decode to structs,  impl response errors

--- server/api/api.go
+++ server/api/api.go
@@ -1,19 +1,61 @@
 package api
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/schema"
 )
 
+type Answers struct {
+	Leeftijd, Studiejaar          int
+	Opleiding, LidRaad, Faculteit string
+	BetrokkenheidOpleiding        int
+	BetrokkenheidHogeschool       int
+	BelangrijkeOnderwerpen        []string
+	CMR                           Medezeggenschapsraad
+	Faculteitsraad                Faculteitsraad
+}
+
+type Medezeggenschapsraad struct {
+	KennisBestaan           string
+	KennisRol               string
+	KrijgtInfo              string
+	SoortInfo               []string
+	MeerWeten               string
+	BeoordelingCommunicatie int
+}
+
+type Faculteitsraad struct {
+	Medezeggenschapsraad
+	KennisVerschilCMR string
+}
+
 func saveAnswers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Content-Type", "application/json")
 	err := r.ParseForm()
 	if err != nil {
-		log.Fatal(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
-	fmt.Println(r.ParseForm())
+	var answers Answers
+	decoder := schema.NewDecoder()
+	err = decoder.Decode(answers, r.PostForm)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		resp := make(map[string]string)
+		resp["message"] = "Data malformed"
+		jsonResp, err := json.Marshal(resp)
+		if err != nil {
+			log.Fatalf("Error in JSON marshal: %s", err)
+		}
+		w.Write(jsonResp)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
 }
 
 func Endpoints(r *mux.Router) {
