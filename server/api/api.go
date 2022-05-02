d0c0e25c078ada6cb0703bec117a4e2416889b3b
qiubee <45637654+qiubee@users.noreply.github.com>
2022-05-02 19:20:49 +0200

    feat: save data to db & remove cors

--- server/api/api.go
+++ server/api/api.go
@@ -1,50 +1,52 @@
 package api
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"studentsurvey/server/db"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/gorilla/schema"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Answers struct {
-	Leeftijd                int                  `schema:"leeftijd"`
-	Studiejaar              int                  `schema:"studiejaar"`
-	Opleiding               string               `schema:"opleiding"`
-	Faculteit               string               `schema:"faculteit"`
-	LidRaad                 string               `schema:"lid-raad"`
-	BetrokkenheidOpleiding  int                  `schema:"betrokkenheid-opleiding"`
-	BetrokkenheidHogeschool int                  `schema:"betrokkenheid-hogeschool"`
-	BelangrijkeOnderwerpen  []string             `schema:"onderwerpen-belangrijk-studie"`
-	CMR                     Medezeggenschapsraad `schema:"cmr"`
-	Faculteitsraad          Faculteitsraad       `schema:"faculteitsraad"`
+	ID                      primitive.ObjectID   `bson:"_id,omitempty"`
+	TimeOfSubmission        primitive.Timestamp  `bson:"time_of_submission"`
+	Leeftijd                int                  `schema:"leeftijd" bson:"leeftijd"`
+	Studiejaar              int                  `schema:"studiejaar" bson:"studiejaar"`
+	Opleiding               string               `schema:"opleiding" bson:"opleiding"`
+	Faculteit               string               `schema:"faculteit" bson:"faculteit"`
+	LidRaad                 string               `schema:"lid-raad" bson:"lid_raad"`
+	BetrokkenheidOpleiding  int                  `schema:"betrokkenheid-opleiding" bson:"betrokkenheid_opleiding"`
+	BetrokkenheidHogeschool int                  `schema:"betrokkenheid-hogeschool" bson:"betrokkenheid_hogeschool"`
+	BelangrijkeOnderwerpen  []string             `schema:"onderwerpen-belangrijk-studie" bson:"belangrijke_onderwerpen_studie,omitempty"`
+	CMR                     Medezeggenschapsraad `schema:"cmr" bson:"centrale_medezeggenschapsraad"`
+	Faculteitsraad          Faculteitsraad       `schema:"faculteitsraad" bson:"faculteitsraad"`
 }
 
 type Medezeggenschapsraad struct {
-	KennisBestaan           string   `schema:"bestaan"`
-	KennisRol               string   `schema:"kennis"`
-	KrijgtInfo              string   `schema:"informatie"`
-	SoortInfo               []string `schema:"soort-informatie"`
-	MeerWeten               string   `schema:"meer-weten"`
-	BeoordelingCommunicatie int      `schema:"communicatie-naar-student"`
+	KennisBestaan           string   `schema:"bestaan" bson:"bestaan"`
+	KennisRol               string   `schema:"kennis" bson:"kennis"`
+	KrijgtInfo              string   `schema:"informatie" bson:"krijgt_info"`
+	SoortInfo               []string `schema:"soort-informatie" bson:"soort_info_cmr,omitempty"`
+	MeerWeten               string   `schema:"meer-weten" bson:"meer_weten"`
+	BeoordelingCommunicatie int      `schema:"communicatie-naar-student" bson:"beoordeling_communicatie"`
 }
 
 type Faculteitsraad struct {
-	KennisBestaan           string   `schema:"bestaan"`
-	KennisRol               string   `schema:"kennis"`
-	KennisVerschilCMR       string   `schema:"kennis-verschil-cmr"`
-	KrijgtInfo              string   `schema:"informatie"`
-	SoortInfo               []string `schema:"soort-informatie"`
-	MeerWeten               string   `schema:"meer-weten"`
-	BeoordelingCommunicatie int      `schema:"communicatie-naar-student"`
+	Medezeggenschapsraad `bson:"inline"`
+	KennisVerschilCMR    string `schema:"kennis-verschil-cmr" bson:"kennis_verschil_cmr"`
 }
 
 func saveAnswers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Set("Content-Type", "application/json")
+
 	err := r.ParseForm()
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
@@ -57,7 +59,7 @@ func saveAnswers(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("error: %v", err)
 		w.WriteHeader(http.StatusBadRequest)
 		resp := make(map[string]string)
-		resp["message"] = "Data malformed"
+		resp["message"] = "Data malformed."
 		jsonResp, err := json.Marshal(resp)
 		if err != nil {
 			log.Fatalf("Error in JSON marshal: %s", err)
@@ -65,11 +67,35 @@ func saveAnswers(w http.ResponseWriter, r *http.Request) {
 		w.Write(jsonResp)
 		return
 	}
+
+	coll := db.Database().Collection("answers")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	_, err = coll.InsertOne(ctx, answers)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		w.WriteHeader(http.StatusBadGateway)
+		resp := make(map[string]string)
+		resp["message"] = "Could not save data."
+		jsonResp, err := json.Marshal(resp)
+		if err != nil {
+			log.Fatalf("Error in JSON marshal: %s", err)
+		}
+		w.Write(jsonResp)
+		return
+	}
+
 	w.WriteHeader(http.StatusCreated)
+	resp := make(map[string]string)
+	resp["message"] = "Data saved."
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalf("Error in JSON marshal: %s", err)
+	}
+	w.Write(jsonResp)
 }
 
 func Endpoints(r *mux.Router) {
 	api := r.PathPrefix("/api/v1").Subrouter()
-	api.HandleFunc("/answers", saveAnswers).Methods(http.MethodPost, http.MethodOptions)
-	api.Use(mux.CORSMethodMiddleware(r))
+	api.HandleFunc("/answers", saveAnswers).Methods(http.MethodPost)
 }
